13c5332cd54d7278076f19580519af40601b470e
Emanuel Rod <emiroker321@gmail.com>
2025-06-10 15:15:18 +0200

    Finished commenting

--- src/htmlparser.py
+++ src/htmlparser.py
@@ -1,49 +1,55 @@
+#Represents the text in between tags
 class Text:
     def __init__(self, text, parent):
         self.text = text
-        self.children = []
+        self.children = [] #Text has no children but is present due to consistancy
         self.parent = parent
     
+    #Method for printing the text
     def __repr__(self):
         return repr(self.text)
 
+#Represents the tags of the HTML code
 class Element:
     def __init__(self, tag, attributes, parent):
         self.tag = tag
-        self.attributes = attributes
+        self.attributes = attributes #Some tags can have attributes like ids, classes...
         self.children = []
         self.parent = parent
 
+    #Method for printing the tags
     def __repr__(self):
         return '<' + self.tag + '>'
 
+#Represents the HTML parser
 class HTMLParser:
     def __init__(self, body):
-        self.body = body
-        self.unfinished = []
+        self.body = body #HTML code
+        self.unfinished = [] #Tags yet to be closed during parsing
         self.SELF_CLOSING_TAGS = [
             'area', 'base', 'br', 'col', 'embed', 'hr', 'img',
             'input', 'link', 'meta', 'param', 'source', 'track',
-            'wbr'
+            'wbr' 
         ]
 
+    #Logic behind parsing
     def parse(self):
         text = ''
         in_tag = False
-        for c in self.body:
-            if c == '<':
-                in_tag = True
-                if text: self.add_text(text)
+        for c in self.body: #Read from body
+            if c == '<': #If in an open tag
+                in_tag = True 
+                if text: self.add_text(text) #Add text element
                 text = ''
             elif c == '>':
                 in_tag = False
-                self.add_tag(text)
+                self.add_tag(text) #Add node element
                 text = ''
             else:
                 text += c
         if not in_tag and text:
-            self.add_text(text)
-        return self.finish()
+            self.add_text(text) #If at end and there's text, add text
+        return self.finish() #Finish the parsing tree
     
     def add_text(self, text):
         if text.isspace(): return
@@ -52,9 +58,9 @@ class HTMLParser:
         parent.children.append(node)
 
     def add_tag(self, tag):
-        tag, attributes = self.get_attributes(tag)
-        if tag.startswith('!'): return
-        if tag.startswith('/'):
+        tag, attributes = self.get_attributes(tag) #Get attributes of tags
+        if tag.startswith('!'): return #Skip tag that start with !
+        if tag.startswith('/'): #Handle finishing tags
             if len(self.unfinished) == 1: return
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
@@ -68,13 +74,15 @@ class HTMLParser:
             node = Element(tag, attributes, parent)
             self.unfinished.append(node)
 
+    #Method to finish the parse tree
     def finish(self):
-        while len(self.unfinished) > 1:
+        while len(self.unfinished) > 1: #Finish all open tags if not finished
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
-            parent.children.append(node)
-        return self.unfinished.pop()
+            parent.children.append(node) #Append to all unfinished tags the last unfinished tag as child
+        return self.unfinished.pop() #Return last tag
     
+    #Get attributes by spliting the tag by spaces and store the key=value into a dictionary 
     def get_attributes(self, text):
         parts = text.split()
         tag = parts[0].casefold()
@@ -85,10 +93,11 @@ class HTMLParser:
                 if len(value) > 2 and value[0] in ['\'', '"']:
                     value = value[1:-1]
                 attributes[key.casefold()] = value
-            else:
+            else: #Handle attributes that don't need value like 'disabled'
                 attributes[attrpair.casefold()] = ''
         return tag, attributes
     
+    #Recursively print the tree
     def print_tree(self, node, indent=0):
         print(' ' * indent, node)
         for child in node.children:
--- src/view.py
+++ src/view.py
@@ -3,29 +3,34 @@ import tkinter.ttk as ttk
 import tkinter.font
 from . import htmlparser as htmlp
 
+#View represents the part of the GUI that manages rendering the website
+#It contains all the elements required for convinient and efficient web rendering like the scrollbar
 class View:
     def __init__(self, root, width, height):
-        self.root = root
-        self.width = width
-        self.height = height
-        self.content = ''
-        self.display_list = []
-        self.HSTEP = 13
-        self.VSTEP = 18
-        self.scroll = 0
-        self.SCROLL_STEP = 50
-        self.page_size = 0
-        self.layout = None
+        self.root = root #A Tkinter window to which a Tkinter canvas will be linked to
+        self.width = width #The width of the view
+        self.height = height #The height of the view
+        self.content = '' #The HTML content the view has to display
+        self.display_list = [] #List of text elements (words) and their position on screen and font (x, y, word, font)
+        self.HSTEP = 13 #The default horizontal distance between words
+        self.VSTEP = 18 #The default vertical distance between lines
+        self.scroll = 0 #The current scroll position (whcih part of the website is being viewd)
+        self.SCROLL_STEP = 50 #The amount of pixels the page moves up or down as we scroll
+        self.page_size = 0 #The vertical page size
+        self.layout = None #Layout of words
 
+    #Loads the view content
     def load(self):
+        #if content is loaded (don't execute when browser first opened)
         if self.content:
-            parser = htmlp.HTMLParser(self.content)
-            self.nodes = parser.parse()
-            self.layout = Layout(self.nodes, self.width, self.height)
+            parser = htmlp.HTMLParser(self.content) #The HTML parser used to read from
+            self.nodes = parser.parse() #Stores the root node
+            self.layout = Layout(self.nodes, self.width, self.height) #Creates the layout based on the root node of the parser and dimentions of the view
             self.display_list = self.layout.display_list
             self.page_size = self.layout.page_size
-            self.render()  
+            self.render() #Renders the content
 
+    #On resize, rerender the page
     def resize(self, event):
         self.height = event.height
         self.width = event.width
@@ -33,6 +38,7 @@ class View:
             self.scroll = max(0, min(self.scroll, self.page_size - self.height))
         self.load()
 
+    #Scrollbar logic !!!See how PySide implements it
     def scrolldown(self, event):
         if self.page_size - self.height > self.scroll:
             self.scroll = max(0, min(self.scroll + self.SCROLL_STEP, self.page_size - self.height))
@@ -71,6 +77,8 @@ class View:
         self.scroll = max(0, min(self.scroll, self.page_size - self.height))
         self.render()
  
+    #Initializes the TKinter canvas and binds key presses for interactivity (scrolling)
+    #Initializes the Tkinter scrollbar
     def create_view(self):
         container = tk.Frame(self.root)
         container.pack(fill='both', expand=True)
@@ -89,34 +97,37 @@ class View:
         self.scrollbar.pack(side='right', fill='y', padx=(0, 2), pady=5)
         self.scrollbar.set(0, 0)
 
+    #Renders the content on the canvas by iterating the display_list
     def render(self):
         self.canvas.delete('all')
         self.update_scrollbar()
         for x, y, c, font in self.display_list:
             if y > self.scroll + self.height: continue
             if y + self.VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c, anchor='nw', font=font)
+            self.canvas.create_text(x, y - self.scroll, text=c, anchor='nw', font=font) #Anchor the text rendering on the top-left side
 
+#Represents the layout of words on screen
 class Layout:
     def __init__(self, nodes, width, height):
         self.display_list = []
-        self.line = []
-        self.fonts = {}
+        self.line = [] #Array of words in a line and their horizontal position, font and vertical placement (x, word, font, placement)
+        self.fonts = {} #A dictionary of fotns to use for caching
         self.width = width
         self.height = height
         self.HSTEP = 13
         self.VSTEP = 18
-        self.cursor_x = self.HSTEP
-        self.cursor_y = self.VSTEP
-        self.weight = 'normal'
+        self.cursor_x = self.HSTEP #Represents the x coordinate for rendering text
+        self.cursor_y = self.VSTEP #Represents the y coordinate for rendering text
+        self.weight = 'normal' 
         self.style = 'roman'
         self.align = 'left'
-        self.placement = 0
-        self.size = 12
-        self.recurse(nodes)
-        if self.display_list:
+        self.placement = 0 #Represents individaul y positions within a line for words
+        self.size = 12 #Font size
+        self.recurse(nodes) #Generate the parse tree for HTML
+        if self.display_list: #Get the vertical page size
             a, self.page_size, b, c = self.display_list[-1]
 
+    #Manages font caching
     def get_font(self, size, weight, style):
         key = (size, weight, style)
         if key not in self.fonts:
@@ -125,29 +136,33 @@ class Layout:
             self.fonts[key] = (font, label)
         return self.fonts[key][0]
 
+    #For each line, determine the layout of each word and move to next line and store it in display_list
     def flush(self, align = 'left'):
-        if not self.line: return
-        metrics = [font.metrics() for x, word, font, placement in self.line]
-        max_ascent = max([metric['ascent'] for metric in metrics])
-        baseline = self.cursor_y + 1.25 * max_ascent
-        line_center = sum(x[0] for x in self.line) / 2
+        if not self.line: return #If line emtpy, return
+        metrics = [font.metrics() for x, word, font, placement in self.line] #Get the metrics (y size) of the font
+        max_ascent = max([metric['ascent'] for metric in metrics]) #Find the max ascent of a line
+        baseline = self.cursor_y + 1.25 * max_ascent #Based on max ascent, find the baseline
+        line_center = sum(x[0] for x in self.line) / 2 #Find the center of the line for center alignment
+        #Fill the display_list
         for x, word, font, placement in self.line:
             y = baseline - font.metrics('ascent') - placement
             if align == 'center' : x += self.width / 2 - line_center / 2 
             self.display_list.append((x, y, word, font))
-        max_descent = max([metric['descent'] for metric in metrics])
-        self.cursor_y = baseline + 1.25 * max_descent
-        self.cursor_x = self.HSTEP
-        self.line = []
+        max_descent = max([metric['descent'] for metric in metrics]) #Find the max descent
+        self.cursor_y = baseline + 1.25 * max_descent #Move the y cursor below the max descent
+        self.cursor_x = self.HSTEP #Reset the x cursor
+        self.line = [] #Reset the line
 
+    #Appends fords to the line array
     def word(self, word):
         font = self.get_font(self.size, self.weight, self.style)
-        w = font.measure(word)
-        if self.cursor_x + w > self.width - self.HSTEP:
+        w = font.measure(word) #Find the width of words based on font
+        if self.cursor_x + w > self.width - self.HSTEP: #If word at end of line, flush it !!!(here add for soft hyphens)
             self.flush()
         self.line.append((self.cursor_x, word, font, self.placement))
-        self.cursor_x += w + font.measure(' ')
+        self.cursor_x += w + font.measure(' ') #Move cursor to the right
 
+    #For soem open HTML tag, perform action
     def open_tag(self, tag):
         if tag == 'i':
             self.style = 'italic'
@@ -163,9 +178,10 @@ class Layout:
             self.align = 'center'
             self.size += 5
         elif tag == 'sup':
-            self.size = round(self.size / 2) # change later so that the resize works for odd numbers too
+            self.size = round(self.size / 2) # !!!Change later so that the resize works for odd numbers too
             self.placement += self.size
 
+    #For some closing HTML tag, perform some action
     def close_tag(self, tag):
         if tag == 'i':
             self.style = 'roman'
@@ -190,6 +206,7 @@ class Layout:
             self.placement -= self.size
             self.size *= 2
 
+    #Recursively traverse the tree to fill the display_list for rendering
     def recurse(self, tree):
         if isinstance(tree, htmlp.Text):
             for word in tree.text.split():
